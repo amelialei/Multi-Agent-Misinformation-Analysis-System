2a8090370817b970ecc3c56b63d2d6cf88cb8c19
amelialei <aclei@ucsd.edu>
2026-01-20 18:33:12 -0800

    sequential agent

--- ff_agents/root_orchestrator/agent.py
+++ ff_agents/root_orchestrator/agent.py
@@ -4,6 +4,7 @@ from ff_agents.frequency_heuristic_agent.agent import freq_heuristic_agent
 from ff_agents.malicious_account_agent.agent import malicious_acc_agent
 from ff_agents.naive_realism_agent.agent import naive_realism_agent
 from ff_agents.sensationalism_agent.agent import sensationalism_agent
+from ff_agents.merger.agent import merger_agent
 
 parallel_analysis_agent = ParallelAgent(
     model='gemini-2.5-flash',
@@ -11,9 +12,10 @@ parallel_analysis_agent = ParallelAgent(
     subagents=[freq_heuristic_agent, malicious_acc_agent, naive_realism_agent, sensationalism_agent],
     description='Runs multiple factuality factor analysis agents in parallel to gather scores and insights on an article.',
 )
-# root_agent = SequentialAgent(
-#     model='gemini-2.5-flash',
-#     name='root_agent',
-#     description='A helpful assistant for user questions.',
-#     instruction='Answer user questions to the best of your knowledge',
-# )
+
+root_agent = SequentialAgent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    sub_agents=[parallel_analysis_agent, merger_agent],
+    description='Coordinates parallel research and synthesizes the results.',
+)
