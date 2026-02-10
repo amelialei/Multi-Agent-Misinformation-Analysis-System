045270bd8b3e2c0df291aa570a04b4be00a3f5e8
amelialei <aclei@ucsd.edu>
2026-02-09 18:25:03 -0800

    fix typo

--- ff_agents/orchestrator/agent.py
+++ ff_agents/orchestrator/agent.py
@@ -2,7 +2,7 @@ from google.adk.agents import ParallelAgent
 from google.adk.agents import SequentialAgent
 from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
-from frequency_heuristic_agent.agent import frequency_heuristic_agent
+from frequency_heuristic_agent.agent import freq_heuristic_agent
 from malicious_account_agent.agent import malicious_acc_agent
 from naive_realism_agent.agent import naive_realism_agent
 from sensationalism_agent.agent import sensationalism_agent
@@ -12,7 +12,7 @@ from claim_extraction_agent.agent import claim_extraction_agent
 parallel_analysis_agent = ParallelAgent(
     name='parallel_analysis_agent',
     sub_agents=[
-        frequency_heuristic_agent, 
+        freq_heuristic_agent, 
         malicious_acc_agent, 
         naive_realism_agent, 
         sensationalism_agent,
